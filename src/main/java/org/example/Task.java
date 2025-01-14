524b729711a00ad633d1bd7903b17fe7d6dbcf7a
CE23768 <erbes-carsten@gmx.de>
2025-01-14 22:20:13 +0100

    Update Task.java

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -5,5 +5,16 @@ package org.example;
 public class Task {
     public int factorial(int input) {
         //write your solution here
-    }
+        if (input==0){
+            fac=1;
+        }//if
+        if (input<0) fac =1;
+        if (input >=1){
+            int fac = 1;
+            for(int 1=1; i<=input; i++){
+            fac=fac*i;
+            }//for
+        }//if
+        return fac;
+    }//factorial
 }
